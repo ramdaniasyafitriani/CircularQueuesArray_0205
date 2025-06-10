a0065a37e5c6aae35cd36c203d16c46a2427cb5b
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-06-10 11:21:55 +0700

    apabila FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array menggunakan perulangan while

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -94,7 +94,13 @@ public :
         }
         else 
         {
-
+            // jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
         }
     }
 };
