5c278cf477403275f7e4b3eb9df0119da5e4d315
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-06-10 11:11:15 +0700

    mengecek apabila antrian hanya memiliki satu elemen menggunakan if else

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -52,5 +52,16 @@ public :
             return;
         }
         cout << "\nThe Element deleted from the queue is : " << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen 
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else 
+        {
+
+        }
     }
 };
