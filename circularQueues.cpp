f5793f8fb360636cdda873e71061a0aedecb4dec
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-06-10 11:11:59 +0700

    apabila elemen yang dihapus berada di posisi terakhir array, kembali ke awal array menggunakan if else

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -61,7 +61,11 @@ public :
         }
         else 
         {
-
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else 
+                FRONT = FRONT + 1;
         }
     }
 };
