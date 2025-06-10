c71a39f1a8ec7b8e7456a4c51d3d67311c4eeb83
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-06-10 10:51:26 +0700

    membuat method void insert

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -12,4 +12,11 @@ public :
         FRONT = -1;
         REAR = -1;
     }
+    void insert()
+    {
+        int num;
+        cout << "Enter a number : ";
+        cin >> num;
+        cout << endl;
+    }
 };
